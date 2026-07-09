662a5cc180c33d16c9d017dc576b49e849f7fa9e
M Hasan <ithasan7@gmail.com>
2026-07-10 02:10:26 +0600

    Deploy to hasanofmars.github.io

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://portfolio.pages.dev",
+  site: "https://hasanofmars.github.io",
   base: "/",
   output: "static",
   server: {
