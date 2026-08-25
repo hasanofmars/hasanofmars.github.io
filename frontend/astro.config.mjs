742f3ad55685a01d2b6754821fd954812ce63385
M Hasan <ithasan7@gmail.com>
2026-08-26 01:03:08 +0600

    Fix quotes in astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,5 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://hasanofmars.github.io",
-  base: "/",
-  output: "static",
-  server: {
-    port: 4321,
-  },
+  site: 'https://hasanofmars.github.io',
 });
